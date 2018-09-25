f4ecf60c26418bb0b5ff738bdb442ce22bbc3322
Lola Onanuga <slonanuga@gmail.com>
2018-09-25 13:43:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 # Add  code here!
 def prime?(n)
 return false if n < 1
-end
   for divisor in (2...Math.sqrt(n)) 
-    if n % divisor == 0 ||  n < 0
+    if n % divisor == 0
   return false
   end
 end
