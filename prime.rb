282384473b0a01ed921f7fd7e7894f153b388225
Lola Onanuga <slonanuga@gmail.com>
2018-09-25 14:08:59 +0000

    complete

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+#return false if n <= 1
+ # #  if n % divisor == 0
+ # return false
+##else return true
+  #end
+##end
+## true
+  
+  
+  return false if num <= 1
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+end
+
+
